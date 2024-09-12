2acb648a474363c3772ec9b532e84851e9ccb07f
Akanle Jeremy <akanlejeremy@gmail.com>
2024-09-12 22:42:32 +0300

    manipulating elements

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+const element = document.createElement("div");
+document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+}
+
+element.append(ul)
+
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Jeremy is the champion";
+element.append(newHeader);
