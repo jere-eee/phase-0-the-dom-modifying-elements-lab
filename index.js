07ad7903da824412bfa978738bb2b67e18cd8f13
Akanle Jeremy <akanlejeremy@gmail.com>
2024-09-30 12:14:24 +0300

    passing

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 // Write your code here!
-window.addEventListener('DOMContentLoaded', () => {
-    const main = document.getElementById("main");
-    main.remove();
+const main = document.querySelector("main#main");
+main.remove();
 
-    const element = document.createElement("div");
-    document.body.append(element);
-    const ul = document.createElement("ul");
-
-    for (let i = 0; i < 3; i++) {
-        const li = document.createElement("li");
-        li.textContent = (i + 1).toString();
-        ul.append(li);
-    }
-
-    element.append(ul)
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Jeremy is the champion";
+element.append(newHeader);
 
 
-    const newHeader = document.createElement("h1");
-    newHeader.id = "victory";
-    newHeader.textContent = "Jeremy is the champion";
-    element.append(newHeader);
-});
+
